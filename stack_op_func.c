a07fe7e07a6f1a3b130e9a98dc0b6537ec454b99
Ayobami <ayobamidele006@gmail.com>
2023-01-18 20:34:40 +0100

    added printall stack operation functionality

--- stack_op_func.c
+++ stack_op_func.c
@@ -3,7 +3,7 @@
 /**
  * _push - push data to the top of a stack
  * @head: the head of a stack double ll
- * @line_number: line number parsed 
+ * @line_number: line number parsed
  */
 void _push(stack_t **head, unsigned int line_number)
 {
@@ -22,4 +22,25 @@ void _push(stack_t **head, unsigned int line_number)
 		(*stack)->prev = top;
 	(*stack) = top;
 }
+
+
+/**
+ * _pall - print all numbers in the stack
+ * @head: head of th stack linked list
+ * @line_number: file line_number
+ */
+void _pall(stack_t **head, unsigned int line_number)
+{
+	(void)line_number;
+	stack_t *tmp;
+
+	tmp = *head;
+
+	while (tmp)
+	{
+		printf("%d", tmp->n);
+		tmp = tmp->next;
+	}
+}
+
 

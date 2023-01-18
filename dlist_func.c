f27681b2261659b5093d06dbe53c982ec18ee12b
Ayobami <ayobamidele006@gmail.com>
2023-01-18 16:58:20 +0100

    added free dlist functionality to free stacked list

---
+++ dlist_func.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - free a `list_t` list
+ * @head: head of linked list
+ */
+
+void free_dlist(stack_t *head)
+{
+	stack_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
